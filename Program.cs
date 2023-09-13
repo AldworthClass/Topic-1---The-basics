6fa056ccdd7dcf128da160fc2d190bc36ca48905
Steve Aldworth <s.aldworth@tvdsb.ca>
2023-09-13 10:01:59 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Topic_1___The_basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            Console.Clear();
+            Console.Title = "MY FANTASTIC PROGRAM";
+            int age;
+            Console.WriteLine("How old are you?");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Next year you will be " + (age + 1));
+
+
+        }
+    }
+}
